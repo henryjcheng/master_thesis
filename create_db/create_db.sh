c981a445b7557db1e0e9ff970113458aeb7f453d
henry <henryjcheng@gmail.com>
2020-03-29 14:09:58 -0700

    add feature: create db, import csv table

--- create_db/create_db.sh
+++ create_db/create_db.sh
@@ -2,4 +2,14 @@
 # need to download the csv files first and unzip
 # website: https://physionet.org/content/mimiciii/1.4/
 
+# follow this solution for creating tables with correct schema
+# https://stackoverflow.com/questions/20240315/how-to-import-csv-file-to-sqlite-with-correct-data-types
+
+rm -rf ../../database
 mkdir ../../database
+
+sqlite3 ../../database/mimic.db \
+    ".mode csv" \
+    ".import "../../data/mimic/D_CPT.csv" d_cpt" \
+    ".table" \
+    ".quit"
