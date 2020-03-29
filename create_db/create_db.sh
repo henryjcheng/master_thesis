5126c98273920e3166ce0b87f5b02eb4c8d0af5e
henry <henryjcheng@gmail.com>
2020-03-29 14:15:21 -0700

    add more import csv file

--- create_db/create_db.sh
+++ create_db/create_db.sh
@@ -11,5 +11,14 @@ mkdir ../../database
 sqlite3 ../../database/mimic.db \
     ".mode csv" \
     ".import "../../data/mimic/D_CPT.csv" d_cpt" \
+    ".import "../../data/mimic/DIAGNOSES_ICD.csv" diagnoses_icd" \
+    ".import "../../data/mimic/D_ICD_DIAGNOSES.csv" d_icd_diagnoses" \
+    ".import "../../data/mimic/D_ICD_PROCEDURES.csv" d_icd_procedures" \
+    ".import "../../data/mimic/D_ITEMS.csv" d_items" \
+    ".import "../../data/mimic/D_LABITEMS.csv" d_labitems" \
+    ".import "../../data/mimic/NOTEEVENTS.csv" noteevents" \
+    ".import "../../data/mimic/PATIENTS.csv" patients" \
+    ".import "../../data/mimic/PRESCRIPTIONS.csv" prescriptions" \
+    ".import "../../data/mimic/PROCEDURES_ICD.csv" procedures_icd" \
     ".table" \
     ".quit"
