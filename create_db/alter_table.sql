30a3c01dde275eb3f33380ff258ad91ad6253902
henry <henryjcheng@gmail.com>
2020-03-29 17:12:52 -0700

    add d_icd_diagnoses, d_icd_procedures, d_items

--- create_db/alter_table.sql
+++ create_db/alter_table.sql
@@ -1,11 +1,10 @@
 -- script to alter the schema for tables in mimic.db
 
 PRAGMA foreign_keys=off;
-
 BEGIN TRANSACTION;
 
+-- ========== d_cpt ==========
 ALTER TABLE d_cpt RENAME TO _d_cpt_original;
-
 CREATE TABLE d_cpt(
   "ROW_ID" INTEGER NOT NULL PRIMARY KEY,
   "CATEGORY" INTEGER,
@@ -17,7 +16,6 @@ CREATE TABLE d_cpt(
   "MINCODEINSUBSECTION" INTEGER,
   "MAXCODEINSUBSECTION" INTEGER
 );
-
 INSERT INTO d_cpt ("ROW_ID", "CATEGORY", "SECTIONRANGE", "SECTIONHEADER",
                    "SUBSECTIONRANGE", "SUBSECTIONHEADER", "CODESUFFIX",
                    "MINCODEINSUBSECTION", "MAXCODEINSUBSECTION")
@@ -25,9 +23,60 @@ INSERT INTO d_cpt ("ROW_ID", "CATEGORY", "SECTIONRANGE", "SECTIONHEADER",
          "SUBSECTIONRANGE", "SUBSECTIONHEADER", "CODESUFFIX",
          "MINCODEINSUBSECTION", "MAXCODEINSUBSECTION"
   FROM _d_cpt_original;
-
 DROP TABLE _d_cpt_original;
 
-COMMIT;
+-- ========== d_icd_diagnoses ==========
+ALTER TABLE d_icd_diagnoses RENAME TO _d_icd_diagnoses_original;
+CREATE TABLE d_icd_diagnoses(
+  "ROW_ID" INTEGER NOT NULL PRIMARY KEY,
+  "ICD9_CODE" TEXT,
+  "SHORT_TITLE" TEXT,
+  "LONG_TITLE" TEXT,
+  FOREIGN KEY ("ICD9_CODE")
+    REFERENCES DIAGNOSES_ICD ("ICD9_CODE")
+);
+INSERT INTO d_icd_diagnoses ("ROW_ID", "ICD9_CODE", "SHORT_TITLE", "LONG_TITLE")
+  SELECT "ROW_ID", "ICD9_CODE", "SHORT_TITLE", "LONG_TITLE"
+  FROM _d_icd_diagnoses_original;
+DROP TABLE _d_icd_diagnoses_original;
 
+-- ========== d_icd_procedures ==========
+ALTER TABLE d_icd_procedures RENAME TO _d_icd_procedures_original;
+CREATE TABLE d_icd_procedures(
+  "ROW_ID" INTEGER NOT NULL PRIMARY KEY,
+  "ICD9_CODE" TEXT,
+  "SHORT_TITLE" TEXT,
+  "LONG_TITLE" TEXT,
+  FOREIGN KEY ("ICD9_CODE")
+    REFERENCES PROCEDURES_ICD ("ICD9_CODE")
+);
+INSERT INTO d_icd_procedures ("ROW_ID", "ICD9_CODE", "SHORT_TITLE", "LONG_TITLE")
+  SELECT "ROW_ID", "ICD9_CODE", "SHORT_TITLE", "LONG_TITLE"
+  FROM _d_icd_procedures_original;
+DROP TABLE _d_icd_procedures_original;
+
+-- ========== d_itmes ==========
+ALTER TABLE d_items RENAME TO _d_items_original;
+CREATE TABLE d_items(
+  "ROW_ID" INTEGER NOT NULL PRIMARY KEY,
+  "ITEMID" INTEGER,
+  "LABEL" TEXT,
+  "ABBREVIATION" TEXT,
+  "DBSOURCE" TEXT,
+  "LINKSTO" TEXT,
+  "CATEGORY" TEXT,
+  "UNITNAME" TEXT,
+  "PARAM_TYPE" TEXT,
+  "CONCEPTID" INTEGER
+);
+INSERT INTO d_items ("ROW_ID", "ITEMID", "LABEL", "ABBREVIATION",
+                     "DBSOURCE", "LINKSTO", "CATEGORY", "UNITNAME",
+                     "PARAM_TYPE", "CONCEPTID")
+  SELECT "ROW_ID", "ITEMID", "LABEL", "ABBREVIATION",
+         "DBSOURCE", "LINKSTO", "CATEGORY", "UNITNAME",
+         "PARAM_TYPE", "CONCEPTID"
+  FROM _d_items_original;
+DROP TABLE _d_items_original;
+
+COMMIT;
 PRAGMA foreign_keys=on;
