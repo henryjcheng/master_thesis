991cdc1cd46c4a1b075d4c727989efaba4cb17e3
henry <henryjcheng@gmail.com>
2020-05-05 09:57:11 -0700

    initial commit

---
+++ create_db/concat_text.py
@@ -0,0 +1,7 @@
+__author__ = "Henry Cheng"
+__email__ = "henryjcheng@gmail.com"
+__status__ = "dev"
+"""
+This script concatenate text in noteevent by HADM_ID.
+This is needed because diagnoses, or the labels, are at HADM_ID level.
+"""
