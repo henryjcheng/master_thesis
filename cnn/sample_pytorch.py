f145fb14dcbf60a40440f13202222bdfe8047544
henry <henryjcheng@gmail.com>
2020-05-08 22:58:37 -0700

    added new layers

--- cnn/sample_pytorch.py
+++ cnn/sample_pytorch.py
@@ -54,17 +54,19 @@ import torch.nn.functional as F
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv1 = nn.Conv2d(3, 6, 2, padding=1)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.conv2 = nn.Conv2d(6, 16, 2, padding=1)
+        self.conv3 = nn.Conv2d(16, 24, 2, padding=1)
+        self.fc1 = nn.Linear(24 * 4 * 4, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
+        x = self.pool(F.relu(self.conv3(x)))
+        x = x.view(-1, 24 * 4 * 4)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
@@ -84,7 +86,7 @@ print(device)
 
 net.to(device)
 
-for epoch in range(2):
+for epoch in range(20):
 
     running_loss = 0.0
     for i, data in enumerate(trainloader, 0):
@@ -101,11 +103,13 @@ for epoch in range(2):
         optimizer.step()
 
         # print statistics
-        running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
-            running_loss = 0.0
+        print(f'epoch: {}')
+        if False:
+            running_loss += loss.item()
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                    (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
 
 print('Finished Training.')
 
