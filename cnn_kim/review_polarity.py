1e78fe2d0e98a975e0bc7d78b0b1ddaef76e2be1
henry <henryjcheng@gmail.com>
2020-05-18 22:49:36 -0700

    add feature: training

--- cnn_kim/review_polarity.py
+++ cnn_kim/review_polarity.py
@@ -25,6 +25,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.autograd import Variable
 
 def clean_str(string, TREC=False):
     """
@@ -181,7 +182,7 @@ if __name__ == "__main__":
     model = Net()
 
     # Define loss and optimization function
-    loss = nn.BCELoss()
+    criterion = nn.BCELoss()
     optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
     # train on GPU
@@ -195,4 +196,24 @@ if __name__ == "__main__":
     train = df[df['fold'] != 10][['embedding', 'polarity']].reset_index(drop=True)
     test = df[df['fold'] == 10][['embedding', 'polarity']].reset_index(drop=True)
 
-    inputs, labels = train['embedding'].sample(frac=1).tolist(), train['polarity'].sample(frac=1).tolist()
+    epoch = 1
+    # train 1 epoch
+    running_loss = 0.0
+    inputs, labels = train['embedding'].cuda(), train['polarity'].cuda()
+
+    # zero the parameter gradients
+    optimizer.zero_grad()
+
+    # forward + backward + optimize
+    outputs = model(inputs)
+    loss = criterion(outputs, labels)
+    loss.backward()
+    optimizer.step()
+
+    # print statistics
+    print(f'epoch: {epoch}')
+    if False:
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print(f'[{epoch+1}, {i+1}] loss: {running_loss/2000}')
+            running_loss = 0.0           
